00ac2e7c063712a9f635ccdc9e8176e8ebc91cf2
Swapnil Gosavi <you@example.com>
2022-11-06 15:52:13 +0530

    code changes for api geteway poc added

---
+++ src/main/java/com/example/springbootapigatewaypoc/SpringBootApiGatewayPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapigatewaypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiGatewayPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiGatewayPocApplication.class, args);
+	}
+
+}
